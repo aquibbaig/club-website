6f6b6bbb790d3b28ba1484b9c44eefdc83a09390
Aquib Baig <aquibbaig97@gmail.com>
2019-05-10 12:15:20 +0530

    remove unused packages

--- src/containers/indexContainer.js
+++ src/containers/indexContainer.js
@@ -1,27 +1,5 @@
 import React from "react";
-import Typography from "@material-ui/core/Typography";
 import Grid from "../components/homepage/grid";
-import MediaQuery from "react-responsive";
-
-const headingText = {
-  fontFamily: `Roboto`,
-  fontSize: `2em`,
-  wordSpacing: `1em`,
-  color: `black`
-};
-
-const hrStyle = {
-  width: `20%`,
-  color: `black`,
-  height: `2px`
-};
-
-const innerPara = {
-  fontSize: `1.125rem`,
-  fontFamily: `Roboto`,
-  marginBottom: `3%`,
-  lineHeight: `2`
-};
 
 const indexContainer = () => (
   <div>
